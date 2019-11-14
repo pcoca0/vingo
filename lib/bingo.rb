e443cb039db7ccda2322b50cc336c3c1a9b55b92
vagrant <vagrant@kleer-csd-box>
2019-11-14 11:00:54 -0300

    sprint2 backEnd

--- lib/bingo.rb
+++ lib/bingo.rb
@@ -1,35 +1,65 @@
 class Bingo
 	def initialize
 		@cartonUno = nil
-		@bolillaSalida=nil 
+		@bolillaSalida=[]
+	    @bolillaPreparada=nil 
 	end
 	
 	def retornarCartonJugador
 	    @cartonUno
 	end
 
-	def asignarCartonJugador param
-	    @cartonUno = param			
+	def asignarCartonJugador
+	    @cartonUno = [[1,"WHITE"],[2,"WHITE"],[3,"WHITE"],[4,"WHITE"],[5,"WHITE"]]			
 	end
 	
 	def retornarBolillaSalida
 	    @bolillaSalida
 	end
 	
-	def sacarBolilla param
-	    @bolillaSalida = param
+	def prepararBolilla param      
+	    @bolillaPreparada = param
+	end
+
+	def sacarBolilla 
+	    if @bolillaPreparada.nil? 
+	       @bolillaSalida.push(rand(5)+1)
+        else
+	       @bolillaSalida.push(@bolillaPreparada)
+	       @bolillaPreparada = nil	
+	    end
+	    			
 	end
 	
 	def verificarCarton
-	    if @bolillaSalida.nil?
+		 if @bolillaSalida == []
 			return ""
 		else
-			if @bolillaSalida == @cartonUno
-				return "GANO"
-			else    
-				return "PERDIO"
-			end		
+			@cartonUno.each{|numero|
+				@existe=false
+				@bolillaSalida.each{|bolilla|
+					if numero[0] == bolilla
+						@existe=true						
+						break			
+					end
+				}
+				if @existe == false
+					return "PERDIO"
+				end				
+			}
+			return "GANO"
 		end
+	end
+
+	def existeBolilla param  
+	    existe=false	   
+	    @bolillaSalida.each{|bolilla|
+		if bolilla == param
+			existe=true
+			break
+		end
+	    }
+	    return existe	
 	end
 
 		
--- spec/vingo_spec.rb
+++ spec/vingo_spec.rb
@@ -7,35 +7,62 @@ describe "Bingo" do
 	expect( bingo.retornarCartonJugador).to eq nil
 	end
 
-	it " Verificar que se asigna el valor 5 al jugador " do
+	it " Verificar que se asigna el valor [1,2,3,4,5]al jugador " do
 	bingo = Bingo.new
-	bingo.asignarCartonJugador 5
-	expect( bingo.retornarCartonJugador ).to eq 5
+	bingo.asignarCartonJugador
+	expect( bingo.retornarCartonJugador ).to eq [[1,"WHITE"],[2,"WHITE"],[3,"WHITE"],[4,"WHITE"],[5,"WHITE"]]
 	end
 	
 	it " Verificar que no haya bolillas fuera del bolillero " do
 	bingo = Bingo.new
-	expect( bingo.retornarBolillaSalida ).to eq nil
+	expect( bingo.retornarBolillaSalida ).to eq []
 	end
 
 	it " Sacar bolilla número 4 " do
 	bingo = Bingo.new
-	bingo.sacarBolilla 4
-	expect( bingo.retornarBolillaSalida ).to eq 4
+        bingo.prepararBolilla 4
+	bingo.sacarBolilla
+	expect( bingo.retornarBolillaSalida ).to eq [4]
 	end
 
-	it " Verificar que jugador gano " do
+	it " Verificar si el jugador gano " do
 	bingo = Bingo.new
-	bingo.asignarCartonJugador 5
-	bingo.sacarBolilla 5
+	bingo.asignarCartonJugador 
+	bingo.prepararBolilla 1
+	bingo.sacarBolilla
+	bingo.prepararBolilla 2
+	bingo.sacarBolilla 
+	bingo.prepararBolilla 3
+	bingo.sacarBolilla 
+	bingo.prepararBolilla 4
+	bingo.sacarBolilla 
+	bingo.prepararBolilla 5
+	bingo.sacarBolilla 
 	expect( bingo.verificarCarton ).to eq "GANO"
 	end
 
 	it " Verificar que jugador perdio " do
 	bingo = Bingo.new
-	bingo.asignarCartonJugador 4
-	bingo.sacarBolilla 5
+	bingo.asignarCartonJugador 
+	bingo.prepararBolilla 1
+	bingo.sacarBolilla
+	bingo.prepararBolilla 2
+	bingo.sacarBolilla 
+	bingo.prepararBolilla 3
+	bingo.sacarBolilla 
+	bingo.prepararBolilla 4
+	bingo.sacarBolilla 
+	bingo.prepararBolilla 6
+	bingo.sacarBolilla  
 	expect( bingo.verificarCarton ).to eq "PERDIO"
+	end
+
+	it " Verificar que la bolilla obtenida no haya salido " do
+	bingo = Bingo.new
+	bingo.asignarCartonJugador
+	bingo.prepararBolilla 6
+	bingo.sacarBolilla
+	expect( bingo.existeBolilla 5 ).to eq false
 	end
 
 
